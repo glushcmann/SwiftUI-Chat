8f2e757e5e7912f052026b761b678ed2e0b0a468
Nick <nikita.glu@icloud.com>
2019-12-02 14:30:02 +0300

    Initial Commit

---
+++ SwiftUI Chat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Chat
+//
+//  Created by Никита on 02.12.2019.
+//  Copyright © 2019 Nikita Glushchenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
